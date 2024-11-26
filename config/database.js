840e4a22b0f32d72bd20f460df276de3900c7253
AgneyaAnoop <agneyaka2002@gmail.com>
2024-11-26 16:50:01 +0530

    Changed mongodb

--- config/database.js
+++ config/database.js
@@ -1,25 +1,13 @@
 const mongoose = require('mongoose');
 
-let cachedConnection = null;
-
 const connectDB = async () => {
-  if (cachedConnection) {
-    return cachedConnection;
-  }
-
   try {
-    const opts = {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    };
+      useUnifiedTopology: true
+    });
 
-    const conn = await mongoose.connect(process.env.MONGODB_URI, opts);
-    cachedConnection = conn;
     console.log('MongoDB connected successfully');
-    return conn;
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
